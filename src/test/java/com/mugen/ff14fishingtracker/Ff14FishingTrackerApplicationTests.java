adfcbab2f74f1468e39d23356d8ca161058dd893
AndrewScalise <paradigmcomplex@gmail.com>
2017-07-10 11:28:24 -0500

    Remade as gradle project

---
+++ src/test/java/com/mugen/ff14fishingtracker/Ff14FishingTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mugen.ff14fishingtracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ff14FishingTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
